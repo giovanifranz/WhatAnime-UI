48d31f4602c38fbcd3066029e53f03f8dcef2a78
giovanifranz <giovanifranz151@gmail.com>
2022-05-01 11:11:57 -0300

    feat: :sparkles: bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-}
+})
