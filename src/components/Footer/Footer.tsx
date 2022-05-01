56b77c7538bf9fda71daa6d01415f965ce51c902
giovani.franz <giovani.franz@agi.com.br>
2022-05-01 15:04:03 -0300

    style: :lipstick: responsive footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Link, Text, VStack } from '@chakra-ui/react'
+import { Box, Flex, Link, Text, useBreakpointValue, VStack } from '@chakra-ui/react'
 import Image from 'next/image'
 
 import { WhatanimeLogo } from '../common'
@@ -6,35 +6,51 @@ import { WhatanimeLogo } from '../common'
 import { Paypal, Social } from '.'
 
 export function Footer() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    md: false,
+    lg: false,
+    xl: true,
+  })
+
   return (
     <Box as="footer" w="full" py="30px" borderTop="1px solid black">
-      <Flex w="70%" mx="auto" alignItems="center">
-        <Box w="35%">
-          <WhatanimeLogo />
+      <Flex w={['95%', '70%']} mx="auto" flexDirection={['column', 'row']}>
+        <Box w={['95%', '35%']} mx="auto" mb={['25px', 'none']}>
+          {isWideVersion && <WhatanimeLogo />}
           <Social />
         </Box>
-        <VStack borderLeft="1px solid black" px="15px" alignItems="left" spacing="20px">
+
+        <VStack
+          borderLeft={['none', '1px solid black']}
+          px="15px"
+          alignItems="left"
+          spacing="25px"
+          width="85%"
+        >
           <Flex alignItems="center">
-            <Text fontSize="xl" fontWeight="200" color="white" w="368px">
+            <Text fontSize="xl" fontWeight="200" color="white" w={['auto', '35%']}>
               This site is our baby, so please consider donating if you are able so that we can keep working
               on it!
             </Text>
-            <Paypal />
+            {isWideVersion && <Paypal />}
           </Flex>
           <Text fontSize="2xl" color="white" textTransform="uppercase">
             Legal
           </Text>
-          <Text fontSize="xl" fontWeight="200" color="white" w="65%">
+          <Text fontSize="xl" fontWeight="200" color="white" w={['100%', '65%']}>
             All anime series names, images, and content are copyrighted content of their respective license
             holders. I do not own the rights to any of these anime series. Anime information compiled from
             AniList and MyAnimeList.
           </Text>
-          <Text fontFamily="Nova Mono" fontSize="2xl" mb="-10px" color="white" textTransform="uppercase">
-            Designed By
-          </Text>
-          <Link href="https://www.highlandertech.com.br/">
-            <Image src="/highlanderLogo.png" alt="HighlanderTech" width={260} height={65} />
-          </Link>
+          <Box>
+            <Text fontFamily="Nova Mono" fontSize="2xl" mb="-10px" color="white" textTransform="uppercase">
+              Designed By
+            </Text>
+            <Link href="https://www.highlandertech.com.br/" mt="-20px" ml="-5px">
+              <Image src="/highlanderLogo.png" alt="HighlanderTech" width={260} height={65} />
+            </Link>
+          </Box>
         </VStack>
       </Flex>
     </Box>
--- src/components/Footer/Social.tsx
+++ src/components/Footer/Social.tsx
@@ -1,28 +1,48 @@
 import { AiFillFacebook, AiOutlineTwitter } from 'react-icons/ai'
 import { SiAnilist, SiMyanimelist } from 'react-icons/si'
-import { HStack, Stack } from '@chakra-ui/react'
+import { HStack, Stack, useBreakpointValue } from '@chakra-ui/react'
 
 import { Link, Text } from '.'
 
+function Share() {
+  return (
+    <>
+      <Link
+        href="https://www.facebook.com/sharer/sharer.php?u=https%3A//www.whatanime.org/"
+        icon={AiFillFacebook}
+      />
+      <Link
+        href="https://twitter.com/intent/tweet?text=https%3A//www.whatanime.org/"
+        icon={AiOutlineTwitter}
+      />
+    </>
+  )
+}
+
 export function Social() {
+  const isMobileVersion = useBreakpointValue({
+    base: true,
+    md: false,
+    lg: false,
+    xl: false,
+  })
+
   return (
     <Stack as="section" spacing="20px">
       <Text title="Connect" />
-      <HStack w="120px" justifyContent="space-between" spacing="10px">
+      <HStack w={['270px', '120px']} justifyContent="space-between" spacing="10px">
         <Link href="https://anilist.co" icon={SiAnilist} />
         <Link href="https://myanimelist.net/profile/HighlanderTech" icon={SiMyanimelist} />
+        {isMobileVersion && <Share />}
       </HStack>
-      <Text title="Share" />
-      <HStack w="120px" justifyContent="space-between" spacing="10px">
-        <Link
-          href="https://www.facebook.com/sharer/sharer.php?u=https%3A//www.whatanime.org/"
-          icon={AiFillFacebook}
-        />
-        <Link
-          href="https://twitter.com/intent/tweet?text=https%3A//www.whatanime.org/"
-          icon={AiOutlineTwitter}
-        />
-      </HStack>
+      {!isMobileVersion && (
+        <>
+          <Text title="Share" />
+          <HStack w="120px" justifyContent="space-between" spacing="10px">
+            <Share />
+          </HStack>
+        </>
+      )}
     </Stack>
   )
 }
