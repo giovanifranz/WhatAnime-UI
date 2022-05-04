840909b548747902c640e8bffcfd31660311a73c
giovani.franz <giovani.franz@agi.com.br>
2022-05-04 00:09:01 -0300

    fix: :iphone: melhorias no footer

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -7,13 +7,10 @@ export function Logo() {
 
   return (
     <Link href="/" passHref>
-      <a className="flex flex-row gap-3 align-middle h-min">
-        <Image
-          src="/logo.svg"
-          width={width < 768 ? 110 : 80}
-          height={width < 768 ? 110 : 80}
-          alt="WhatAnime"
-        />
+      <a className="flex flex-row gap-3 mb-2">
+        <div className="relative lg:w-20 lg:h-20 w-44 h-44">
+          <Image src="/logo.svg" layout="fill" alt="WhatAnime" />
+        </div>
         {width > 768 && (
           <h1 className="font-mono text-4xl text-yellow-300 font-normal w-40 uppercase">What Anime?</h1>
         )}
