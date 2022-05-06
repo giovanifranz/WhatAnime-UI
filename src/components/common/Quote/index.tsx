efe7142624b042ce9378c7016760d30e55087547
giovanifranz <giovanifranz151@gmail.com>
2022-05-06 02:52:49 -0300

    fix: :bug: button quote

--- src/components/common/Quote/index.tsx
+++ src/components/common/Quote/index.tsx
@@ -15,7 +15,7 @@ export function QuoteComponent() {
     <section className="relative h-40 p-2 bg-yellow-300 rounded w-36 md:w-64">
       <p className="w-full text-lg italic font-light line-clamp-3">“{data.quote}”</p>
       <div className="absolute flex items-center justify-between w-full pr-2 bottom-1">
-        <div className="flex flex-col justify-between w-3/4 h-12">
+        <div className="flex flex-col justify-between w-2/3 h-12 md:w-3/4">
           <h4 className="text-lg font-bold line-clamp-1">“{data.character}”</h4>
           <p className="text-lg line-clamp-1">“{data.title}”</p>
         </div>
