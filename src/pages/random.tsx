4d95e693ed9f7966750a364711d2b2aab133da77
giovanifranz <giovanifranz151@gmail.com>
2022-06-03 02:18:46 -0300

    feat: cache in random page

--- src/pages/random.tsx
+++ src/pages/random.tsx
@@ -1,12 +1,29 @@
 import { useAnimeRandom } from 'hooks'
+import { GetServerSideProps } from 'next'
 import { useRouter } from 'next/router'
+import { IAnime } from 'types'
 
 import { AnimeTemplate } from 'components/anime/Template'
+import { getAnimeRandom } from 'utils/http'
 
-export default function AnimePage() {
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  res.setHeader('Cache-Control', 's-maxage=59, stale-while-revalidate')
+
+  return {
+    props: {
+      randomAnime: await getAnimeRandom(),
+    },
+  }
+}
+
+interface Props {
+  randomAnime: IAnime
+}
+
+export default function AnimePage({ randomAnime }: Props) {
   const router = useRouter()
 
-  const { isLoading, isError, data } = useAnimeRandom()
+  const { isLoading, isError, data } = useAnimeRandom(randomAnime)
 
   if (isLoading || !data) {
     return null
