2a6ea9d106c907f374466785afc11a4ba2af56f6
giovanifranz <giovanifranz151@gmail.com>
2022-06-21 00:31:20 -0300

    refactor: add PropsWithChildren

--- src/components/common/atoms/Link.tsx
+++ src/components/common/atoms/Link.tsx
@@ -1,12 +1,11 @@
-import { memo, ReactNode } from 'react'
+import { memo, PropsWithChildren } from 'react'
 import NextLink from 'next/link'
 
 interface Props {
-  children: ReactNode
   href: string
 }
 
-function LinkComponent({ children, href }: Props) {
+function LinkComponent({ href, children }: PropsWithChildren<Props>) {
   return (
     <NextLink href={href} passHref>
       <a className="flex p-1 rounded-lg h-14 w-14 bg-slate-100 text-zinc-400 dark:text-neutral-800 hover:bg-yellow-300 hover:text-neutral-800 transition">
